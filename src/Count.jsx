4c07155dc951cdc72cdc40f7d13f1698e42b6e77
Longodios <tusaromix@gmail.com>
2024-05-22 09:51:14 +0200

    Añado funcionalidad hasta horas

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React , {useState , useEffect} from "react";
 import Number from "./Number";
 
 
 
 
 
-const Count = ({ms}) => {
+const Count = () => {
+
+    const  [ms , SetMs] = useState(0);
+    const [decimal , SetDecimal] = useState(0);
+    const [hour , SetHour ]  = useState(0);
+    const [day , setDay] = useState(0);
+   
+   useEffect(() => {
+        const interval = setInterval(()=> {
+           SetMs((prevMs => {
+               if (prevMs === 9){
+                   SetDecimal((prevDecimal) => prevDecimal + 1);
+                   return 0;
+               } else  {
+                   return prevMs + 1;
+               }
+           }))
+
+           SetDecimal ((prevDec => {
+            if (prevDec === 9){
+                SetHour((prevHour) => prevHour + 1);
+                return 0;
+            } else {
+                return prevDec;
+            }
+           }))
+           
+            
+        }, 1000);
+        return () => clearInterval(interval);
+   },[]);
+
+
     return (
         <>
         <div className="container-fluid flex h-32 bg-black w-screen justify-center gap-5">
-            <Number ms = {ms}/>
-            <Number ms = {ms}/>
             <Number/>
             <Number/>
-            <Number/>
-            <Number/>
+            <Number value = {day}/>
+            <Number value = {hour}/>
+            <Number value = {decimal}/>
+            <Number value = {ms}/>
         </div>
         </>
     )
--- src/Number.jsx
+++ src/Number.jsx
@@ -4,28 +4,21 @@ import PropTypes from 'prop-types';
 
 
 
-const Number = () => {
+const Number = ({value}) => {
      //Inicializamos la variable
-     const  [ms , SetMs] = useState(0);
-    
-    useEffect(() => {
-         const interval = setInterval(()=> {
-             SetMs(prevMs => prevMs + 1);
-         }, 1000);
-         return () => clearInterval(interval);
-    },[]);
+   
 
    
     return (
        <div className="border-4 text-center w-20 flex justify-center ">
-        <span className="font-bold mt-10 text-5xl text-white" >{ms}</span>
+        <span className="font-bold mt-10 text-5xl text-white" >{value}</span>
        
        </div>
     )
 }
 
 Number.propTypes = {
-    ms : PropTypes.number
+   value: PropTypes.number
 }
 
     
